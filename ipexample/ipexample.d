1d02fb52446f37cb5570bb5366f7f89bd1539aa4
Theo Julienne <theo.julienne@gmail.com>
2009-09-30 09:23:16 +0000

    awesomeness

--- ipexample/ipexample.d
+++ ipexample/ipexample.d
@@ -2,6 +2,7 @@ module ipexample;
 
 import std.string;
 import std.stdio;
+import std.math;
 
 import icypixels.all;
 
@@ -14,6 +15,8 @@ class IPExample {
     
     Texture tex;
     
+    StopWatch timer;
+    
     this( ) {
         win = new GLWindow( 1024, 768 );
         win.title = "IcyPixels Example";
@@ -25,6 +28,8 @@ class IPExample {
 		win.onRedraw += &handleRedraw;
 		
 		tex = TextureCache.loadTexture( "images/test.png", false );
+		
+		timer.start;
     }
     
     void handleMouseMove( GLWindow window, Event event ) {
@@ -52,7 +57,29 @@ class IPExample {
 	    
 	    glLoadIdentity( );
 	    
-	    Primitives.renderSprite( tex.width, tex.height, tex );
+	    double t = cast(double)timer.microsec / 1000000;
+	    
+	    t *= 2;
+	    
+	    //double x = sin(t) * 100;
+	    //double y = cos(t) * 100;
+	    for ( int i = 0; i < 100; i++ ) {
+	        double z = sin(t);
+	        
+	        double x = i / 10;
+	        double y = i % 10;
+	        
+	        x += sin( t ) * 0.5;
+	        y += cos( t ) * 0.5;
+	        
+	        glLoadIdentity( );
+	        glTranslatef( window.width/10 * x, window.height/10 * y, 0 );
+	        glRotatef( cos(t*cast(double)i/100)*50, 0.0f, 0.0f, 1.0f );
+	        glScalef( z, z, z );
+	        glColor4f( abs(sin(t)), abs(cos(t)), abs(tan(t)), 1.0f );
+	        //glTranslatef( 100+x, 100+y, 0 );
+	        Primitives.renderSprite( tex.width, tex.height, tex );
+	    }
 	    
 	    win.resetPerspectiveProjection( );
 	}
