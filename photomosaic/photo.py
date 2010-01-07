b8d3ea710d244c5d4d933d4f6e4280ab3d022b06
goldy <goldy@goldy>
2010-01-07 15:03:14 +0000

    much more user friendly. edits to both download.py and photo.py

--- photomosaic/photo.py
+++ photomosaic/photo.py
@@ -6,28 +6,35 @@
 # FLICKR API SECRET: ad9e2f8d4c18fc87
 
 import PIL.Image as Image
+import glob, os
 
-AMOUNT_OF_IMAGES = 100
-INFINITY = float('inf')
-
-subImageWidth = 30
-subImageHeight = 30
-superImage = 'failfan.jpg'
-
+def findNumImages():
+    maxNum = -1
+    for imagefile in glob.glob('images/*.jpg'):
+        maxNum = max([maxNum, int(os.path.basename(imagefile).split('.')[0])])
+    return maxNum+1
 
 
-def getAverageColour(image):
+AMOUNT_OF_IMAGES = findNumImages()
+superImage = 'aj.jpg'
+resolution = 5 # The lower, the less subimages will be visible (down to 1)
+pixelConversionFactor = 8  # Each pixel in super-image represents 10 pixels of a sub-image
+                           # This represents the 'size
+
+subImageDimension = resolution * pixelConversionFactor
+
+def getAverageRegionColour(image, (startx, starty, endx, endy)):
     tr = tg = tb = 0
-    #print image.size
-    width, height = image.size
+    width = endx - startx
+    height = endy - starty
 
-    for i in range(height):
-        for j in range(width):
-            px = image.getpixel((i,j))
+    for y in range(starty, endy):
+        for x in range(startx, endx):
+            px = image.getpixel((x,y))
             tr += px[0]
             tg += px[1]
             tb += px[2]
-	    
+
     return tr/(width*height), tg/(width*height), tb/(width*height)
 
 
@@ -38,7 +45,7 @@ def colourOffset(c1, c2):
 
 
 def getMostSimilarImage(targetColour):
-    smallestOffset = INFINITY
+    smallestOffset = float('inf')
     bestImage = None
     for i in range(AMOUNT_OF_IMAGES):
         if colourOffset(averageColours[i], targetColour) < smallestOffset:
@@ -47,36 +54,42 @@ def getMostSimilarImage(targetColour):
     return Image.open('images/%d.jpg' % bestImage)
 
 
-
+print 'Preprocessing image database for colour information'
 averageColours = []
 for i in range(AMOUNT_OF_IMAGES):
-    print 'Average colour calculation of image', i
-    averageColours.append(getAverageColour(Image.open('images/%d.jpg' % i)))
+    print ('%.1f' % ((i*100)/float(AMOUNT_OF_IMAGES))) + '% complete.'
+    img = Image.open('images/%d.jpg' % i)
+    averageColour = getAverageRegionColour(img, (0, 0, img.size[0], img.size[1]))
+    averageColours.append(averageColour)
+print 'Completion of image database processing.\n'
 
-print 'Completion of average colour calculation.'
 
-target = Image.open(superImage)
+print 'Opening super-image...'
+superImage = Image.open(superImage)
+imageWidth, imageHeight = superImage.size
+print 'Super-image opened up successfully.'
 
-imageWidth, imageHeight = target.size
 
-print 'Super-image opened up.'
+print 'Initialising mosaic canvas...'
+mosaicWidth =  pixelConversionFactor * imageWidth
+mosaicHeight = pixelConversionFactor * imageHeight
 
-mosaic = Image.new('RGB', (subImageWidth*imageWidth,subImageHeight*imageHeight), 'White')
-print 'Mosaic image canvas formed with dimensions %d*%d' % (subImageWidth*imageWidth, subImageHeight*imageHeight)
+mosaic = Image.new('RGB', (mosaicWidth, mosaicHeight), 'White')
+print 'Mosaic canvas formed with dimensions %dx%d\n' % (mosaicWidth, mosaicHeight)
 
-print 'Beginning creation of mosaic...'
-
-for y in range(imageHeight):
+print 'Beginning formation of mosaic...'
+for y in range(0, imageHeight-resolution, resolution):
     print ('%.1f' % ((y*100)/float(imageHeight))) + '% complete.'
-    
-    for x in range(imageWidth):
-        targetColour = target.getpixel((x, y))
-        bestMatch = getMostSimilarImage(targetColour)
+
+    for x in range(0, imageWidth-resolution, resolution):
+        regionColour = getAverageRegionColour(superImage, (x, y, x+resolution, y+resolution))
+        subImage = getMostSimilarImage(regionColour)
         
-        subImage = bestMatch.copy()
-       # print x*subImageWidth, y*subImageHeight, x*(subImageWidth+1), y*(subImageWidth+1)
-        mosaic.paste(subImage, (x*subImageWidth, y*subImageHeight, x*subImageWidth+subImageWidth, y*subImageHeight+subImageWidth))
+        subImage = subImage.resize((subImageDimension, subImageDimension))
 
-print 'Render complete! See mosaic.jpg for result.'
+        mosaic.paste(subImage, (x/resolution*subImageDimension, y/resolution*subImageDimension, x/resolution*subImageDimension+subImageDimension, y/resolution*subImageDimension+subImageDimension))
 
+print 'Mosaic formation is complete. Rendering process initialising...'
 mosaic.save('mosaic.jpg')
+
+print 'Rendering complete! See result in mosaic.jpg'
